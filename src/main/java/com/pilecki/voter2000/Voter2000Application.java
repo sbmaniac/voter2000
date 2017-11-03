1bcfd37e1cc9f8ce0d99c7db50e110206c46fac6
sbmaniac-laptop\sbmaniac <pilecki.maurycy@gmail.com>
2017-11-03 15:27:04 +0100

    Initial commit

---
+++ src/main/java/com/pilecki/voter2000/Voter2000Application.java
@@ -0,0 +1,12 @@
+package com.pilecki.voter2000;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Voter2000Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Voter2000Application.class, args);
+	}
+}
